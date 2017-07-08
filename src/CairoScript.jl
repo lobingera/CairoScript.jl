2da9fa72e452d9c67cdae2289d28d36f93b4d5a7
lobingera <lobingera@gmail.com>
2017-07-08 14:15:18 +0200

    something for testing, compat(string

--- src/CairoScript.jl
+++ src/CairoScript.jl
@@ -4,6 +4,7 @@ depsjl = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
 isfile(depsjl) ? include(depsjl) : error("CairoScript not properly ",
     "installed. Please run\nPkg.build(\"CairoScript\")")
 
+using Compat;  import Compat.String
 using Cairo
 importall Graphics
 
--- src/calls.jl
+++ src/calls.jl
@@ -11,9 +11,9 @@ function interpreter_install_hooks(c::Interpreter,h::InterpreterHooks)
     c
 end
 
-function interpreter_run(c::Interpreter,filename::String)
+function interpreter_run(c::Interpreter,filename::AbstractString)
     ccall((:cairo_script_interpreter_run,_jl_libcsi),
-        UInt64,(Ptr{Void},Ptr{UInt8}),c.ptr,string(filename))
+        UInt64,(Ptr{Void},Ptr{UInt8}),c.ptr,@compat(String(filename)))
 end
 
 #function interpreter_feed_stream(c::Interpreter,stream::IOStream)
@@ -23,7 +23,7 @@ end
 
 function interpreter_feed_string(c::Interpreter,s::AbstractString)
     ccall((:cairo_script_interpreter_feed_string,_jl_libcsi),
-        UInt64,(Ptr{Void},Ptr{UInt8}),c.ptr,s)
+        UInt64,(Ptr{Void},Ptr{UInt8}),c.ptr,@compat(String(s)))
 end
 
 
